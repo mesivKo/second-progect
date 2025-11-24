161929a8871843d96c02f43b7dab1b5e39986371
mesivKo <doshi07.08@gmail.com>
2025-11-24 20:34:33 +0300

    commit

--- my-app/src/pages/Login/Login.tsx
+++
@@ -1,3 +0,0 @@
-export function Login(){
-    return <h2>Login page</h2>;
-}
---
+++ my-app/src/pages/Login/LoginPage.tsx
@@ -0,0 +1,7 @@
+export function Login(){
+    return <div>
+        
+        <h2>Добро пожаловать</h2>
+        
+        </div>;
+}
